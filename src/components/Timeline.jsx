cfcf1015ea80eaa31446665e4b2feed253b0952b
martindimmen <martindimmen@proton.me>
2024-01-03 04:34:59 +0100

    timeline added

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import timeline from "../data/timeline";
+import TimelineItem from "./TimeineItem";
 
-function Timeline(){
-    return(
-        <div>Timeline</div>
-    )
+function Timeline() {
+  return (
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        {timeline.map((item) => (
+          <TimelineItem
+            year={item.year}
+            title={item.title}
+            duration={item.duration}
+            details={item.details}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Timeline;
---
+++ src/data/timeline.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    year: "Today",
+    title: "Software Engineer",
+    duration: "newly graduated",
+    details: "Yeet lorem ipsum yeet",
+  },
+  {
+    year: "2023",
+    title: "Software Engineer",
+    duration: "newly graduated",
+    details: "Yeet lorem ipsum yeet",
+  },
+  {
+    year: "Today",
+    title: "Software Engineer",
+    duration: "newly graduated",
+    details: "Yeet lorem ipsum yeet",
+  },
+  {
+    year: "Today",
+    title: "Software Engineer",
+    duration: "newly graduated",
+    details: "Yeet lorem ipsum yeet",
+  },
+];
