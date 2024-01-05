28c72f1ed56f7de17acee396212c687c81a89676
martindimmen <martindimmen@proton.me>
2024-01-05 22:04:20 +0100

    added more in timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,21 +1,22 @@
 export default [
   {
-    year: "Today",
-    title: "Software Engineer",
-    duration: "newly graduated",
+    year: "2023",
+    title: "Board member, UiS Subsea",
+    duration: "10.2023-",
     details: "Yeet lorem ipsum yeet",
   },
   {
     year: "2023",
-    title: "Software Engineer",
-    duration: "newly graduated",
-    details: "Yeet lorem ipsum yeet",
+    title: "Vice President, Stavanger AI(STAI)",
+    duration: "04.2023-",
+    details:
+      " Working to promote artificial intelligence among students at the University of Stavanger.",
   },
   {
-    year: "Today",
-    title: "Software Engineer",
-    duration: "newly graduated",
-    details: "Yeet lorem ipsum yeet",
+    year: "2023",
+    title: "Bachelor in Software Engineering",
+    duration: "2023",
+    details: "Finished my bachelor after writing my thesis for UiS Subsea",
   },
   {
     year: "Today",
