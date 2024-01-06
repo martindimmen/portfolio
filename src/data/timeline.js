2d586f36882e7332b0ce56cd8cb9a0720d5a08d4
martindimmen <martindimmen@proton.me>
2024-01-06 21:42:07 +0100

    Added UiS Subsea

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,12 +1,13 @@
 export default [
   {
-    year: "2023",
+    year: "Currently",
     title: "Board member, UiS Subsea",
     duration: "10.2023-",
-    details: "Yeet lorem ipsum yeet",
+    details:
+      "Yeet lorem ipsum yeetBoard Member, UiS Subsea As a board member at UiS Subsea, I have a supervisory role ensuring all other board members perform their tasks effectively. Additionally, I step in supportively when needed and have the honorable responsibility of guiding this year's cohort of data bachelor students in their ROV task",
   },
   {
-    year: "2023",
+    year: "Currently",
     title: "Vice President, Stavanger AI(STAI)",
     duration: "04.2023-",
     details:
