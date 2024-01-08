3bd6ea256abf659e03c88731ebf2a447947ba07d
martindimmen <martindimmen@proton.me>
2024-01-08 10:05:05 +0100

    timeline changes

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,22 +1,22 @@
 export default [
   {
-    year: "Currently",
+    year: "2023",
     title: "Board member, UiS Subsea",
-    duration: "10.2023-",
+    duration: "Currently",
     details:
       "Yeet lorem ipsum yeetBoard Member, UiS Subsea As a board member at UiS Subsea, I have a supervisory role ensuring all other board members perform their tasks effectively. Additionally, I step in supportively when needed and have the honorable responsibility of guiding this year's cohort of data bachelor students in their ROV task",
   },
   {
-    year: "Currently",
+    year: "2023",
     title: "Vice President, Stavanger AI(STAI)",
-    duration: "04.2023-",
+    duration: "Currently",
     details:
       "Working to promote artificial intelligence among students at the University of Stavanger.",
   },
   {
-    year: "2021-2023",
+    year: "2021,2022,2023",
     title: "Teaching Assistant",
-    duration: "Years",
+    duration: "3 Semesters",
     details: "Teaching Assistant in dat120 basic programming",
   },
   {
